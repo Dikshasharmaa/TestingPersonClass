4c000302ce81c0d2c67b252e5b958919824160a8
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-06 22:13:35 -0500

    Finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,84 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name ="";
+    private int age = Integer.MAX_VALUE ;
+    private int height = Integer.MAX_VALUE;
+    private int weight = Integer.MAX_VALUE;
+    private String lastName = "";
+    private String favColor = "";
 
     public Person() {
     }
+    public Person(String name, int age, int height, int weight, String lastName, String favColor){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.lastName = lastName;
+        this.favColor = favColor;
+    }
+
 
     public Person(int age) {
+        this.age=age;
     }
 
     public Person(String name) {
+        this.name=name;
     }
 
     public Person(String name, int age) {
+        this.name=name;
+        this.age=age;
     }
 
+
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age=age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFavColor(String favColor) {
+        this.favColor = favColor;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFavColor() {
+        return favColor;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,12 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        Integer expectedHeight = Integer.MAX_VALUE;
+        Integer expectedWeight = Integer.MAX_VALUE;
+        String expectedLastName = "";
+        String expectedFavColor = "";
+
+
 
         // When
         Person person = new Person();
@@ -19,9 +25,18 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        Integer actualHeight = person.getHeight();
+        Integer actualWeight = person.getWeight();
+        String actualLastName = person.getLastName();
+        String actualFavColor = person.getFavColor();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedWeight, actualWeight);
+        Assert.assertEquals(expectedLastName, actualLastName);
+        Assert.assertEquals(expectedFavColor, actualFavColor);
+
     }
 
     @Test
@@ -50,7 +65,6 @@ public class TestPerson {
         Assert.assertEquals(expected, actual);
     }
 
-
     @Test
     public void testConstructorWithNameAndAge() {
         // Given
@@ -66,6 +80,36 @@ public class TestPerson {
 
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testConstructorWithAllValues() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+        Integer expectedHeight = 5;
+        Integer expectedWeight = 50;
+        String expectedLastName = "Smith";
+        String expectedFavColor = "Blue";
+
+        // When
+        Person person = new Person(expectedName,expectedAge,expectedHeight,expectedWeight,expectedLastName,expectedFavColor);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualHeight = person.getHeight();
+        Integer actualWeight = person.getWeight();
+        String actualLastName = person.getLastName();
+        String actualFavColor = person.getFavColor();
+
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedWeight, actualWeight);
+        Assert.assertEquals(expectedLastName, actualLastName);
+        Assert.assertEquals(expectedFavColor, actualFavColor);
     }
 
     @Test
@@ -93,6 +137,61 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setHeight(expected);
+
+        // Then
+        Integer actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 50;
+
+        // When
+        person.setWeight(expected);
+
+        // Then
+        Integer actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetLastName() {
+        // Given
+        Person person = new Person();
+        String expected = "Smith";
+
+        // When
+        person.setLastName(expected);
+        String actual = person.getLastName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetFavColor() {
+        // Given
+        Person person = new Person();
+        String expected = "Blue";
+
+        // When
+        person.setFavColor(expected);
+        String actual = person.getFavColor();
+
+        // Then
         Assert.assertEquals(expected, actual);
     }
 }
